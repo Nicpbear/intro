c0c55198b107034d57a4c32b3df8210677b90fa8
Nicpbear <pepee5815@gmail.com>
2025-02-27 10:12:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
+from PIL import Image
+
 st.title("Mi Primera App!!")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo reaizar backend y frontend")
+image = Image.open(Cr7 cr7.jpeg")
+
+st.image(image, caption="Interfaces multimodales")
