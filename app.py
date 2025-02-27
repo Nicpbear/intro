9deb2a2d168ecbbf0bfc41989bf468a722e59de1
Nicpbear <pepee5815@gmail.com>
2025-02-27 10:27:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ st.write("Facilmente puedo reaizar backend y frontend")
 image = Image.open("cr7.jpeg")
 
 st.image(image, caption="Interfaces multimodales")
+
+
+texto ) st.text_input("Oe")
+st.write("Que más")
+
+st.subheader("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col2:
+  st.subheader("Segunda columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ("Visual","Auditiva","Táctil"))
+  if modo == "Visual":
+    st.write("La vista es fundamental para tu interfaz")
+  if modo == "Auditivo":
+    st.write("La audición es fundamental para tu interfaz")
